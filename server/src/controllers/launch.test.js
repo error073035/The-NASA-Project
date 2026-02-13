91f2e64ff30cdfdbba547eea72ced3951d04c83a
Kahan Hirani <kahanhirani073@gmail.com>
2026-02-13 11:51:39 +0530

    Test: test launch file

---
+++ server/src/controllers/launch.test.js
@@ -0,0 +1,23 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', async () => {
+    const res = 200;
+    expect(res).toBe(200);
+  });
+});
+
+describe('Test POST /launches', () => {
+  test('It should respond with 200 success', async () => {
+    const res = 200;
+    expect(res).toBe(200);
+  });
+
+  test('It should catch missing required properties', async () => {
+    const res = 400;
+    expect(res).toBe(400);
+  });
+
+  test('It should catch invalid dates', async () => {
+    const res = 400;
+    expect(res).toBe(400);
+  });
+});
